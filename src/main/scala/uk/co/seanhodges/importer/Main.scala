4022f488a89c30717c75221f2b7ef3fabf04ec88
sean <shodges@wav2.com>
2017-01-09 15:53:12 +0000

    Updated the DI and added a quick and dirty CLI to invoke the new backup/restore operations

--- src/main/scala/uk/co/seanhodges/importer/Main.scala
+++ src/main/scala/uk/co/seanhodges/importer/Main.scala
@@ -13,19 +13,34 @@ object Main extends App with ModuleInjection {
   private val logger = LoggerFactory.getLogger(getClass.getName)
 
   if (args.isEmpty) {
-    val program = new Exception().getStackTrace.head.getFileName
-    logger.info(s"Please support a target XML file: $program [FILENAME]")
-    logger.info(s"")
-    System.exit(1)
+    // Run a Datastore backup
+    backupDS.start().end
   }
-  if (!File(args(0)).exists) {
+  else if (File(args(0)).exists) {
+    val xml = new XMLEventReader(Source.fromFile(args(0), "UTF-8"))
+
+    // Restore a given backup file
+    // TODO: Read ahead and determine backup type from contents
+    if (args(0).contains("datastore-backup")) {
+      // Restore a datastore backup
+      restoreDS.start(xml).end
+    }
+    else if (args(0).contains("wordpress")) {
+      // Restore a wordpress backup
+      restoreWP.start(xml).end
+    }
+    else {
+      val filePath = args(0)
+      logger.info(s"Unknown backup format")
+      System.exit(1)
+    }
+  }
+  else {
+    // Bad path provided
     val filePath = args(0)
     logger.info(s"Target XML file $filePath does not exist")
     System.exit(1)
   }
-
-  val xml = new XMLEventReader(Source.fromFile(args(0), "UTF-8"))
-  consumer.start(xml).end
 
   logger.info(s"Import finished successfully")
 }
--- src/main/scala/uk/co/seanhodges/importer/ModuleInjection.scala
+++ src/main/scala/uk/co/seanhodges/importer/ModuleInjection.scala
@@ -1,19 +1,26 @@
 package uk.co.seanhodges.importer
 
 import org.slf4j.LoggerFactory
-import uk.co.seanhodges.importer.consumer.WPContentConsumer
-import uk.co.seanhodges.importer.parser.WPXMLParser
-import uk.co.seanhodges.importer.writer.GAEDatastoreWriter
+import uk.co.seanhodges.importer.consumer.{BackupXMLContentConsumer, DatastoreContentConsumer, WPXMLContentConsumer}
+import uk.co.seanhodges.importer.parser.{DatastoreParser, XMLParser}
+import uk.co.seanhodges.importer.writer.{XMLContentWriter, DatastoreWriter}
 
 /**
  * Created by sean on 19/12/16.
  */
 trait ModuleInjection {
 
-  // Use of lazy to avoid instantiation if the module is never used
+  object BackupDeps {
+    lazy val parser = new DatastoreParser()
+    lazy val writer = new XMLContentWriter()
+  }
+  lazy val backupDS = new DatastoreContentConsumer(BackupDeps.parser, BackupDeps.writer)
 
-  lazy val parser = new WPXMLParser()
-  lazy val writer = new GAEDatastoreWriter()
-  lazy val consumer = new WPContentConsumer(parser, writer)
+  object RestoreDeps {
+    lazy val parser = new XMLParser()
+    lazy val writer = new DatastoreWriter()
+  }
+  lazy val restoreWP = new WPXMLContentConsumer(RestoreDeps.parser, RestoreDeps.writer)
+  lazy val restoreDS = new BackupXMLContentConsumer(RestoreDeps.parser, RestoreDeps.writer)
 
 }
